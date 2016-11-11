88c77b996386b852491fb45167fc4a5bddd24a7b
zachnurc <zach.nurcombe@gmail.com>
2016-11-11 14:46:00 +0000

    changed position of language selector

--- partials/_sidebar.php
+++ partials/_sidebar.php
@@ -1,4 +1,5 @@
 <div id="sidebar">
+  <a href="/">Dashboard</a>
   <section>
     <h4>Problems</h4>
     <a href="/problems">All</a>
@@ -13,10 +14,10 @@
     <a href="/users#specialists">Specialists</a>
     <a href="/users#admins">Admins</a>
   </section>
-  <section>
-    <h4>Third Section</h4>
-    <a href="#">Menu Item x</a>
-    <a href="#">Look at me</a>
-    <a href="#">Nothing here</a>
-  </section>
+  <div id="language_selector">
+    <a href="https://make-it-all-demo.herokuapp.com"><img src="assets/images/eng-flag.png"/></a>
+    <a href="https://make-it-all-demo-arabic.herokuapp.com"><img src="assets/images/arabic-flag.png"/></a>
+    <a href="https://make-it-all-demo-german.herokuapp.com"><img src="assets/images/german-flag.png"/></a>
+    <a href="https://make-it-all-demo-chinese.herokuapp.com"><img src="assets/images/chinese-flag.png"/></a>
+  </div>
 </div>
